55dcd5e2831aefba23df236146dfb60a6fc8bad9
spencers20 <spencernyaenya@gmail.com>
2025-07-06 19:14:08 +0300

    checking on the agent again

--- pages/api/query.ts
+++ pages/api/query.ts
@@ -12,13 +12,13 @@ type flowisepayload={
 type  chatflowisepayload={
     question:string,
     namespace:string,
-    chatId:string
+    thread_id:string
 }
 export async function callflowise(data:flowisepayload |chatflowisepayload ) {
     try {
-      const response = await fetch(process.env.API_KEY as string, {
+      const response = await fetch(process.env.LEXI_URL as string, {
         method: "POST",
-        headers: {
+        headers: {  
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
@@ -41,7 +41,8 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
   try {
     const cookies=parse(req.headers.cookie||"")
-    const namespace = cookies.namespace
+    // const namespace = cookies.namespace
+    const namespace="lexi-78d9c735"
     if (!namespace) {
       return res.status(500).json({
         error: "Namespace not available. Please upload the document again to proceed.",
@@ -63,14 +64,14 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
       const results = await callflowise(flowisepayload);
       if (results) {
         chatId = results.chatId;
-        console.log("results", results.text);
-        return res.status(200).json({ response: results.text });
+        console.log("results", results.response);
+        return res.status(200).json({ response: results.response });
       }
     } else {
       const chatflowisepayload : chatflowisepayload = {
         question: query,
         namespace:namespace,
-        chatId: chatId,
+        thread_id: chatId,
       };
       const results = await callflowise(chatflowisepayload);
       return res.status(200).json({ response: results.text });
